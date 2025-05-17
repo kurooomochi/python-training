487101356d77388b5444b2a9f69f7dfad69b4240
kurooomochi <santiagozapatabohorquez@hotmail.com>
2025-05-17 00:52:06 -0500

    feat: Refactor main.py to remove command-line argument parsing and integrate CLIHandler for task management

--- task-tracker/main.py
+++ task-tracker/main.py
@@ -1,45 +1,30 @@
-import argparse
+# main.py
+import sys
+import os
 
-def add_task(args):
-    print('adding task...')
-    print(f'task description {args.description}')
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), 'src')))
 
-def delete_task(args):
-    print('removing task...')
-    print(f'task id to remove {args.id}')
 
-def update_task(args):
-    print('updating task...')
-    print(f'task id to update {args.id}')
-
+from src.application.TodoService_adapter import TodoService
+from src.infrastructure.persistence.TaskMemoryRepository_adapter import TaskMemoryRepository
+from src.infrastructure.cli.handler import CLIHandler
 
 def main():
-    parser = argparse.ArgumentParser(
-        prog="task-tracker",
-        description="Task Tracker application",
-    )
-    subparsers = parser.add_subparsers(title="Commands", dest="command", required=True, help="Available Commands")
+    """
+    Main function to set up and run the To-Do CLI application.
+    This is the composition root of the application, where dependencies are wired together.
+    """
+    # 1. Choose and initialize the Driven Adapter (Persistence)
+    task_repository = TaskMemoryRepository()
 
-    # Add command
-    parser_add = subparsers.add_parser("add", help="Adds a new task.")
-    parser_add.add_argument("description", type=str, help="Task description.")
-    parser_add.set_defaults(func=add_task)
+    # 2. Initialize the Application Core (Service)
+    todo_service = TodoService(repository=task_repository)
 
-    # Delete command
-    parser_add = subparsers.add_parser("delete", help="Removes a task.")
-    parser_add.add_argument("id", type=int, help="Task id.")
-    parser_add.set_defaults(func=delete_task)
-    
-    # Update command
-    parser_add = subparsers.add_parser("update", help="Updates a task.")
-    parser_add.add_argument("id", type=int, help="Task id.")
-    parser_add.set_defaults(func=update_task)
+    # 3. Initialize the Driving Adapter (CLI)
+    cli_handler = CLIHandler(service=todo_service)
 
-    args = parser.parse_args()
-    if hasattr(args, 'func'):
-        args.func(args) # Pasamos las tareas cargadas a la función del comando
-    else:
-        parser.print_help() # Si no se proporciona un subcomando válido
+    # 4. Run the application by letting the CLI handler process command-line arguments
+    cli_handler.handle()
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
